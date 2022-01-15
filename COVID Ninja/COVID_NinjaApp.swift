9c6913a110928c297fb98d9b25f00be2e9871609
magmal <lena.malczewska277@gmail.com>
2022-01-15 10:56:21 +0100

    Initial Commit

---
+++ COVID Ninja/COVID_NinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_NinjaApp.swift
+//  COVID Ninja
+//
+//  Created by Magda Malczewska on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct COVID_NinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
